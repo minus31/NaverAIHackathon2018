5d16a11aa8e7d203587c6a33872dd698521a5030
wjy5446 <whaledev.yoon@gmail.com>
2019-01-06 19:45:15 +0900

    text

--- main_jy.py
+++ main_jy.py
@@ -22,26 +22,67 @@ def bind_model(model):
         torch.save(model.state_dict(), os.path.join(dir_name, 'model.pth'))
         print('model saved!')
 
-    def load(dir_name):
-        print(dir_name)
-        model.load_state_dict(os.path.join(dir_name, "model.pth"))
+    def load(file_path):
+        dir = file_path
+        checkpoint = torch.load(dir, map_location=lambda storage, loc: storage)
+        model.load_state_dict(checkpoint)        
         print('model loaded!')
 
     def infer(queries, db):
         # Query 개수: 195
         # Reference(DB) 개수: 1,127
         # Total (query + reference): 1,322
-
+        
         queries, query_img, references, reference_img = preprocess(queries, db)
 
         print('test data load queries {} query_img {} references {} reference_img {}'.
               format(len(queries), len(query_img), len(references), len(reference_img)))
-        print(type(reference_img))
 
-        # get image features
+        # immediate model
+        model_fc = model.features
+        model_features = nn.Sequential(list(model.classifier.children())[0])
 
-        
+        transforms_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])            
+            ])
 
+        def hamming2(s1, s2):
+            """Calculate the Hamming distance between two bit strings"""
+            assert len(s1) == len(s2)
+            return sum(c1 != c2 for c1, c2 in zip(s1, s2))
+
+        # get feature of queries
+        dict_query_features = {}
+        for path, img in zip(queries, query_img):
+            img_pre = transforms_test(img)
+            output = model_fc(img_pre.cuda().unsqueeze(0))
+            output = output.flatten()
+            output = model_features(output)
+            dict_query_features[path.split('/')[-1].split('.')[0]] = "".join((output > 0).cpu().detach().numpy().astype('str'))
+
+        # get feature of references
+        dict_reference_features = {}
+        for path, img in zip(references, reference_img):
+            img_pre = transforms_test(img)
+            output = model_fc(img_pre.cuda().unsqueeze(0))
+            output = output.flatten()
+            output = model_features(output)
+            dict_reference_features[path.split('/')[-1].split('.')[0]] = "".join((output > 0).cpu().detach().numpy().astype('str'))
+
+        # make list
+        li_result = []
+
+        for query_key, query_features in dict_query_features.items():
+            dict_hamming = {}
+            for reference_key, reference_features in dict_reference_features.items():
+                dict_hamming[reference_key] = hamming2(query_features, reference_features)
+            
+            sorted_hamming = sorted(dict_hamming.items(), key=lambda x: x[1])
+            sorted_hamming = [x[0] for x in sorted_hamming]
+            li_result.append((query_key, sorted_hamming))
+
+        return list(zip(range(len(li_result)), li_result))        
 
     nsml.bind(save=save, load=load, infer=infer)
 
@@ -77,26 +118,13 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import torchvision.utils as utils
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        
-        self.resnet = models.vgg16(pretrained=False)
-        self.resnet.fc = nn.Linear(in_features=2048, out_features=1000, bias=True)
-        self.softmax = nn.Softmax(dim=1)
-        
-    def forward(self, x):
-        x = self.resnet(x)
-        out = self.softmax(x)
-        return out
-
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
     # hyperparameters
-    args.add_argument('--epochs', type=int, default=2)
-    args.add_argument('--batch_size', type=int, default=256)
+    args.add_argument('--epochs', type=int, default=20)
+    args.add_argument('--batch_size', type=int, default=64)
 
     # DONOTCHANGE: They are reserved for nsml
     args.add_argument('--mode', type=str, default='train', help='submit일때 해당값이 test로 설정됩니다.')
@@ -113,22 +141,20 @@ if __name__ == '__main__':
     # ```Model```
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    model = models.vgg16(pretrained=False).to(device)
+    model = models.vgg16(pretrained=True).to(device)
     bind_model(model)
-    nsml.save(0)
-    nsml.load(checkpoint='0')
 
     if config.pause:
         nsml.paused(scope=locals())
 
     bTrainmode = False
 
-    if config.mode == 'tarain':
+    if config.mode == 'train':
         bTrainmode = True
 
         """ set loss and optim"""
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.resnet.fc.parameters(), lr=0.0002, weight_decay=0.0005)
+        optimizer = optim.Adam(model.classifier.parameters(), lr=0.0002, weight_decay=0.0005)
 
         """ Load data """
         transform = transforms.Compose([
@@ -141,8 +167,7 @@ if __name__ == '__main__':
         train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
         
         """ Training Model"""
-        nsml.load(checkpoint='0')
-
+        
         for epoch in range(nb_epoch):
             total_batch = len(train_loader)
 
